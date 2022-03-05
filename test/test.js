567b757c4d46f36647e5cbd34f6d83d6f2b6b343
sonianb <sonia.botsarova@gmail.com>
2022-03-05 14:10:42 +0000

    add test for addItems function

---
+++ test/test.js
@@ -0,0 +1,14 @@
+//------------------------Testing addItems()--------------------------------------
+
+test("addItems() should add a new object to the toDolist array", () => {
+
+})
+
+// test("addItems() should read user's input value", () => {
+
+// })
+
+test("addItems() should modify the length of the toDolist array", () => {
+
+})
+ 
