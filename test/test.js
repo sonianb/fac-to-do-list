b926ec83b71426a0727b63111a20d7b938dc09be
sonianb <sonia.botsarova@gmail.com>
2022-03-07 14:04:19 +0000

    test if the original array has the same elements except deleted ones

--- test/test.js
+++ test/test.js
@@ -17,5 +17,15 @@ test("addItem() should add a new object to the toDos array", () => {
     equal(test1.toDos.length, 2)
     test1.deleteItem("water the plants")
     equal(test1.toDos.length, 1)
-    equal(test1.toDos[0].task, "walk the dog")
+})
+
+test("with deleteItem() the original array will have the same elements except the removed ones", () => {
+    const test2 = new ToDoList();
+    test2.addItem("Go for a walk"); //add a new element 
+    equal(test2.toDos.length, 3) //check if new element has been added
+    equal(test2.toDos[2].task, "Go for a walk") //check if the correct element has been added
+    test2.deleteItem(2) //delete the new element
+    equal(test2.toDos[0].task, "walk the dog") //check if new array has same elements 
+    equal(test2.toDos[1].task, "water the plants")
+    equal(test2.toDos[2], undefined)
 })
