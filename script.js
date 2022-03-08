3dc6b129931273a56a492d850d6f2d33e4c7a6e3
sonianb <sonia.botsarova@gmail.com>
2022-03-08 22:45:06 +0000

    add falsy parameter to constructor because tests don't pass

--- script.js
+++ script.js
@@ -5,9 +5,16 @@ const submitBtn = document.getElementById('submit-btn')
 
 //data structure
 class ToDoList {
-    constructor() {
+
+    /**
+     * Constructor creates a to-do list using local storage, if needed.
+     * If items exist in local storage, initialise toDos using the saved values from local storage, if it doesn't create empty array.
+     * @param {*} useLocalStorage boolean, determines whether local storage should be used
+     */
+
+    constructor(useLocalStorage) {
         const tempTodo = localStorage.getItem('to-do-list')
-        if (tempTodo) { //if item exists, do JSONparse to transform to obj to initialise in toDos
+        if (useLocalStorage && tempTodo) { //if item exists, do JSONparse to transform to obj to initialise in toDos
             this.toDos = JSON.parse(tempTodo);
         } else {
             this.toDos = [];
@@ -16,9 +23,11 @@ class ToDoList {
 
     addItem(taskContent) {
         this.toDos.push({task: taskContent, done: false, date: new Date()});
+        this.saveToLocalStorage();
     }
     deleteItem(index) {
         this.toDos.splice(index, 1);
+        this.saveToLocalStorage();
     }
     getDoneItems() {
         return this.toDos.filter(elem => elem.done === true)
@@ -28,13 +37,14 @@ class ToDoList {
     }
     toggleItemDone(index) {
         this.toDos[index]["done"] = !this.toDos[index]["done"];
+        this.saveToLocalStorage();
     }
     saveToLocalStorage() {
         window.localStorage.setItem("to-do-list", JSON.stringify(this.toDos))
     }
 }
 
-const toDoList = new ToDoList();
+const toDoList = new ToDoList(true);
 
 // Joe: update display/DOM
 
