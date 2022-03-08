d4417e5fc6c7bc5b13d95b412bf4e28072156b64
joe <joe.dev.public@gmail.com>
2022-03-08 22:10:08 +0000

    Added eventListeners, plenty of comments explaining stuff
    Relates #20

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ class ToDoList {
         this.toDos.push({task: taskContent, done: false, date: new Date()});
     }
     deleteItem(index) {
-       this.toDos.splice(index, 1); 
+        this.toDos.splice(index, 1);
     }
     getDoneItems() {
         return this.toDos.filter(elem => elem.done === true)
@@ -33,7 +33,7 @@ class ToDoList {
        return this.toDos;
     }
     toggleItemDone(index) {
-        this.toDos[index]["done"] = !this.toDos[index]["done"];        
+        this.toDos[index]["done"] = !this.toDos[index]["done"];
     }
 }
 
@@ -67,48 +67,96 @@ const toDoList = new ToDoList();
 */
 
 function updateDisplay(list) {
+
     const outputElement = document.querySelector('section');
     const items = list.getAllItems();
-    //let html = '';
+    outputElement.innerHTML = '';
+
     for (const item of items) {
 
-        const newTaskDiv = document.createElement('div');
+        const taskContainerEl = document.createElement('div');
 
-        const newDoneCheckbox = document.createElement('input');
-        newDoneCheckbox.setAttribute('type', 'checkbox');
+        const toggleDoneInputEl = document.createElement('input');
+        toggleDoneInputEl.setAttribute('type', 'checkbox');
 
-        const newTaskTextDiv = document.createElement('span'); // change to div...
-        newTaskTextDiv.innerHTML = item.task;
+        const taskTextContainerEl = document.createElement('span'); // change to div?
+        // Conditionally wrap the text in a strikethrough tag:
+        //if (item['done'] === true) {
+        //    taskTextContainerEl.innerHTML = `<s>${item['task']}</s>`;
+        //} else {
+            taskTextContainerEl.innerHTML = item['task'];
+        //}
 
-        const newDeleteButton = document.createElement('input');
-        newDeleteButton.setAttribute('type', 'button');
-        newDeleteButton.setAttribute('value', 'X');
+        const deleteButtonEl = document.createElement('input');
+        deleteButtonEl.setAttribute('type', 'button');
+        deleteButtonEl.setAttribute('value', 'X');
 
-        newTaskDiv.append(newDoneCheckbox);
-        newTaskDiv.append(newTaskTextDiv);
-        newTaskDiv.append(newDeleteButton);
+        taskContainerEl.append(toggleDoneInputEl);
+        taskContainerEl.append(taskTextContainerEl);
+        taskContainerEl.append(deleteButtonEl);
 
-        outputElement.append(newTaskDiv);
+        outputElement.append(taskContainerEl);
 
-        // Now you can add event listeners for the checkbox and button at the same time
+        /*  Get the index in the array of the current item:
+            (indexOf *seems* to be a safe/reliable way to do this?
+            i.e. it won't confuse "superficially identical" elements.)
+        */
+        const currentItemIndex = items.indexOf(item);
 
-/*
-        html += `
-<div class="task">
-    <input type="checkbox">
-    ${item.task}
-    <input type="button" value="X">
-</div>
-`;
-*/
+        /*  If you want to have each eventListener function complete by calling
+            some function to "update the DOM", you'll probably need to have
+            an "updateTask" function (which takes an item as a parameter),
+            which would then be called by updateDisplay for each item in a given list.
+            (This means that the eventListener functions can ask to just "redraw"
+            that one task, then the item is changed, rather than redraw the entire list.)
+
+            For now: just manipulate the DOM immediately and directly!
+        */
+
+        // Contents of these arrow functions could be moved to separate (named) functions:
+
+        toggleDoneInputEl.addEventListener("input", () => {
+            /*  You can address the item directly if you change the class method to:
+
+                toggleItemDone(item) {
+                    item["done"] = !item["done"];
+                }
+
+                And call it like this here:
+
+                list.toggleItemDone(item);
+
+                But for now, just use the established index method.
+            */
+            list.toggleItemDone(currentItemIndex);
+            // Add the "done" class to this task's text container:
+            taskTextContainerEl.classList.toggle('done');
+            // Conditionally wrap the text in a strikethrough tag:
+            if (item['done'] === true) {
+                taskTextContainerEl.innerHTML = `<s>${taskTextContainerEl.innerHTML}</s>`;
+            } else {
+                taskTextContainerEl.innerHTML = taskTextContainerEl.innerText;
+            }
+            // Is calling the whole parent function from here a good idea?!
+            //updateDisplay(list);
+        });
+
+        deleteButtonEl.addEventListener("click", () => {
+            taskContainerEl.remove();
+            list.deleteItem(currentItemIndex);
+            //updateDisplay(list);
+        });
+
     }
-    // outputElement.innerHTML = html;
+
 }
 
+// Temporary test data:
 const testList = new ToDoList();
 
 testList.addItem("Hello world");
 testList.addItem("Clear out the garden shed");
 testList.addItem("Just a test item");
+testList.addItem("Hello world"); // intentional duplicate
 
 updateDisplay(testList);
